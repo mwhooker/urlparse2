576bb6965d56ed7b33f79345425218866ae41550
Matthew Hooker <mwhooker@gmail.com>
2012-03-11 13:50:13 -0700

    first test.

---
+++ tests/test_usability.py
@@ -0,0 +1,15 @@
+import urlparse2
+from unittest import TestCase
+
+
+class TestUsability(TestCase):
+
+    def test_mutability(self):
+        url = 'http://www.cwi.nl:80/%7Eguido/Python.html'
+        expect = 'https://www.cwi.nl:80/%7Eguido/Python.html'
+
+        result = urlparse2.urlparse(url)
+        result.scheme = 'https'
+        
+        joined = urlparse2.urljoin(result)
+        self.AssertEquals(url, expect)
