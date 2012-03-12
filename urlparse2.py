b3acce17ae3f319f25427c1429ed948749c4ebc1
jeremy avnet <brainsik-github@theory.org>
2012-03-12 00:16:27 -0700

    get all up in sys.modules junk

--- urlparse2.py
+++ urlparse2.py
@@ -1,11 +1,10 @@
 # encoding: utf-8
+import sys
 from recordtype import recordtype
-urlparse1 = __import__('urlparse')
 
-for name in (n for n in urlparse1.__dict__.iterkeys()):
-    if name.startswith('__') or name == 'ParseResult':
-        continue
-    globals()[name] = getattr(urlparse1, name)
+import urlparse as urlparse1
+from urlparse import *
+del sys.modules['urlparse']
 
 
 class ParseResult(recordtype('ParseResult', 'scheme netloc path params query fragment'), urlparse1.ResultMixin):
@@ -26,6 +25,7 @@ class SplitResult(recordtype('SplitResult', 'scheme netloc path query fragment')
 
 urlparse1.ParseResult = ParseResult
 urlparse1.SplitResult = SplitResult
+
 
 def urljoin(base, url, allow_fragments=True):
     parsed = urlparse1.urlparse(base)
