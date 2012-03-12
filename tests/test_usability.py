72bdf648087ed0ebc641fe79feaaff47232d6961
Matt Chisholm <matt@theory.org>
2012-03-11 22:43:12 -0700

    tests for non-idiotic urljoin behavior

--- tests/test_usability.py
+++ tests/test_usability.py
@@ -13,3 +13,19 @@ class TestUsability(TestCase):
 
         joined = urlparse2.urlunparse(result)
         self.assertEquals(joined, expect)
+
+    def test_protocolless_urlprefix(self):
+        prefix = 'domain.com'
+        path = 'path/to/file'
+        expect = 'domain.com/path/to/file'
+
+        result = urlparse2.urljoin(prefix, path)
+        self.assertEquals(result, expect)
+
+    def test_protocolfull_urlprefix(self):
+        prefix = 'http://domain.com'
+        path = 'path/to/file'
+        expect = 'http://domain.com/path/to/file'
+
+        result = urlparse2.urljoin(prefix, path)
+        self.assertEquals(result, expect)
